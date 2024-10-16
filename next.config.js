a7cacf9a776e889418a1bd3a6a2d9f655379c313
sweetbanana-dev <corey.gray.dev@gmail.com>
2024-10-15 17:39:17 -0700

    ddd

--- next.config.js
+++ next.config.js
@@ -3,32 +3,10 @@
  * for Docker builds.
  */
 await import("./src/env.js");
-import WithPWA from "next-pwa";
-
-const withPWA = WithPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  scope: "/",
-  sw: "service-worker.js",
-});
-
 /**
  * @type {import('next').NextConfig}
  */
 // @ts-ignore
-const config = withPWA({
-  reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-});
+const config = {};
 
 export default config;
